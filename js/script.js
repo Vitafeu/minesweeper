8763af997a45ac0b0e39d41cadaf0c35673f3fd9
Vitafeu <benji.feu.feu.feu@gmail.com>
2022-11-16 14:47:34 +0100

    added a function to init the game

--- js/script.js
+++ js/script.js
@@ -1,22 +1,50 @@
-// ---------------------------- Constants ----------------------------
-
-const sizeX = 100;
-const sizeY = 100;
-
-const mines = 100;
-
 // ---------------------------- Functions ----------------------------
 
 function createTable(tableData) {
-    var table = document.createElement('table');
-    var tableBody = document.createElement('tbody');
+    let table = document.createElement('table');
+    let tableBody = document.createElement('tbody');
 
     tableData.forEach(function(rowData) {
-        var row = document.createElement('tr');
+        let row = document.createElement('tr');
 
         rowData.forEach(function(cellData) {
-            var cell = document.createElement('td');
-            cell.appendChild(document.createTextNode(cellData));
+            let cell = document.createElement('td');
+
+            if (cellData == "X") {
+                cell.classList.add("mine");
+            } else if (cellData != "0") {
+                cell.appendChild(document.createTextNode(cellData));
+            }
+
+            switch (cellData) {
+                case "1":
+                    cell.classList.add("blue");
+                    break;
+                case "2":
+                    cell.classList.add("green");
+                    break;
+                case "3":
+                    cell.classList.add("red");
+                    break;
+                case "4":
+                    cell.classList.add("purple");
+                    break;
+                case "5":
+                    cell.classList.add("maroon");
+                    break;
+                case "6":
+                    cell.classList.add("turquoise");
+                    break;
+                case "7":
+                    cell.classList.add("black");
+                    break;
+                case "8":
+                    cell.classList.add("gray");
+                    break;
+                default:
+                    break;
+            }
+
             row.appendChild(cell);
         });
 
@@ -27,90 +55,106 @@ function createTable(tableData) {
     document.body.appendChild(table);
 }
 
+function createGrid(sizeY, sizeX, mines) {
+    
+    let grid = new Array(sizeY);
 
-// ---------------------------- Code ----------------------------
+    for (let y = 0; y < grid.length; y++) {
+        grid[y] = new Array(sizeX);
 
-var grid = new Array(sizeY);
-
-for (let y = 0; y < grid.length; y++) {
-    grid[y] = new Array(sizeX);
-
-    for (let x = 0; x < grid[y].length; x++) {
-        grid[y][x] = "0";
-    }
-}
-
-for (let i = 0; i < mines; i++) {
-    let randX = Math.floor(Math.random() * sizeX);
-    let randY = Math.floor(Math.random() * sizeY);
-
-    while (grid[randY][randX] == "X") {
-        randX = Math.floor(Math.random() * sizeX);
-        randY = Math.floor(Math.random() * sizeY);
-    }
-
-    grid[randY][randX] = "X";
-}
-
-for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-        
-        if (grid[y][x] != "X") {
-            let nearMines = 0;
-
-            if (y != 0) {
-                if (x != 0) {
-                    if (grid[y - 1][x - 1] == "X") {
-                        nearMines += 1;
-                    }
-                }
-                
-                if (grid[y - 1][x] == "X") {
-                    nearMines += 1;
-                }
-        
-                if (x != sizeX) {
-                    if (grid[y - 1][x + 1] == "X") {
-                        nearMines += 1;
-                    }
-                }
-                
-            }  
-            // --------------------------------
-            if (x != 0) {
-                if (grid[y][x - 1] == "X") {
-                    nearMines += 1;
-                }
-            }
-            
-
-            if (x != sizeX) {
-                if (grid[y][x + 1] == "X") {
-                    nearMines += 1;
-                }
-            }
-            // --------------------------------
-            if (y != (sizeY - 1)) {
-                if (x != 0) {
-                    if (grid[y + 1][x - 1] == "X") {
-                        nearMines += 1;
-                    }
-                }
-                
-                if (grid[y + 1][x] == "X") {
-                    nearMines += 1;
-                }
-
-                if (x != sizeX) {
-                    if (grid[y + 1][x + 1] == "X") {
-                        nearMines += 1;
-                    }    
-                }
-            }
-        
-            grid[y][x] = nearMines.toString();
+        for (let x = 0; x < grid[y].length; x++) {
+            grid[y][x] = "0";
         }
-    } 
+    }
+
+    for (let i = 0; i < mines; i++) {
+        let randX = Math.floor(Math.random() * sizeX);
+        let randY = Math.floor(Math.random() * sizeY);
+
+        while (grid[randY][randX] == "X") {
+            randX = Math.floor(Math.random() * sizeX);
+            randY = Math.floor(Math.random() * sizeY);
+        }
+
+        grid[randY][randX] = "X";
+    }
+
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            
+            if (grid[y][x] != "X") {
+                let nearMines = 0;
+
+                if (y != 0) {
+                    if (x != 0) {
+                        if (grid[y - 1][x - 1] == "X") {
+                            nearMines += 1;
+                        }
+                    }
+                    
+                    if (grid[y - 1][x] == "X") {
+                        nearMines += 1;
+                    }
+            
+                    if (x != sizeX) {
+                        if (grid[y - 1][x + 1] == "X") {
+                            nearMines += 1;
+                        }
+                    }
+                    
+                }  
+                // --------------------------------
+                if (x != 0) {
+                    if (grid[y][x - 1] == "X") {
+                        nearMines += 1;
+                    }
+                }
+                
+
+                if (x != sizeX) {
+                    if (grid[y][x + 1] == "X") {
+                        nearMines += 1;
+                    }
+                }
+                // --------------------------------
+                if (y != (sizeY - 1)) {
+                    if (x != 0) {
+                        if (grid[y + 1][x - 1] == "X") {
+                            nearMines += 1;
+                        }
+                    }
+                    
+                    if (grid[y + 1][x] == "X") {
+                        nearMines += 1;
+                    }
+
+                    if (x != sizeX) {
+                        if (grid[y + 1][x + 1] == "X") {
+                            nearMines += 1;
+                        }    
+                    }
+                }
+            
+                grid[y][x] = nearMines.toString();
+            }
+        } 
+    }
+
+    createTable(grid);
 }
 
-createTable(grid);
+function initGame(difficulty) {
+    switch (difficulty) {
+        case 'begginer':
+            createGrid(9, 9, 10);
+            break;
+        case 'intermediate':
+            createGrid(16, 16, 40);
+            break;
+        case 'expert':
+            createGrid(16, 30, 99);
+            break;
+        default:
+            break;
+    }
+}
