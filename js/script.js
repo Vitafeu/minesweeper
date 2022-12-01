84824087f6c547d7e98531fc9992d8b78bedc5ee
Vitafeu <benji.feu.feu.feu@gmail.com>
2022-12-01 14:14:09 +0100

    added win verification and a timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+// ---------------------------- Variables ----------------------------
+
+var fullTime;
+var timer;
+
 // ---------------------------- Functions ----------------------------
 
 function createTable(tableData) {
     let table = document.createElement('table');
-    table.id = "minesweeperGrid";
     let tableBody = document.createElement('tbody');
 
     tableData.forEach(function(rowData) {
@@ -68,6 +72,8 @@ function createTable(tableData) {
 
     table.appendChild(tableBody);
     document.body.appendChild(table);
+
+    createHTMLTimer();
 }
 
 function createGrid(sizeY, sizeX, mines) {
@@ -165,11 +171,28 @@ function destroyGrid() {
     for (let i = 0; i < grids.length; i++) {
         grids[i].remove();
     }
+
+    clearInterval(timer);
+    try {
+        timeDiv.remove();
+    } catch (error) {
+        
+    }
+    
 }
 
-function initGame(difficulty) {
+function initGame() {
 
     destroyGrid();
+
+    let inputs = document.getElementsByTagName('input');
+    let difficulty;
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "radio" && inputs[i].checked) {
+            difficulty = inputs[i].value;
+        }
+    }
 
     switch (difficulty) {
         case 'begginer':
@@ -235,14 +258,108 @@ function revealNearZeros(element) {
 
 function winCheck() {
     let grid = document.getElementsByTagName("table")[0];
+    let win = true;
 
     for (var i = 0, r = grid.rows.length; i < r; i++) {
         for (var j = 0, c = grid.rows[i].cells.length; j < c; j++) {
             if (grid.rows[i].cells[j].textContent != "X" && grid.rows[i].cells[j].classList.contains("tile")) {
-                return;
-            }
-
-            //console.log("You win !")
+                win = false;
+            }          
         }
+    }
+
+    if (win) {
+        setTimeout(function(){
+            alert("You win !")
+            destroyGrid();
+            setBestTime();
+            win = false;
+        }, 100);
+    }
+}
+
+function startTimer() {
+    let seconds = 0;
+    let minutes = 0;
+    timer = setInterval(function(){
+        
+        if (seconds >= 59) {
+            seconds = 0;
+            minutes += 1;
+        } else {
+            seconds += 1;
+        }
+
+        if (minutes < 10) {
+            if (seconds < 10) {
+                fullTime = "0" + minutes.toString() + ":" + "0" + seconds.toString();
+            } else {
+                fullTime = "0" + minutes.toString() + ":" + seconds.toString();
+            }  
+        } else {
+            if (seconds < 10) {
+                fullTime = minutes.toString() + ":" + "0" + seconds.toString();
+            } else {
+                fullTime = minutes.toString() + ":" + seconds.toString();
+            }
+        }
+
+        time.innerHTML = fullTime;
+    }, 1000);
+}
+
+function createHTMLTimer() {
+    let times = document.createElement('div');
+    let HTMLTimer = document.createElement('p');
+    let timerIcon = document.createElement('i');
+
+    times.id = "timeDiv";
+    timerIcon.classList.add('fa-solid', 'fa-stopwatch');
+    HTMLTimer.id = "time";
+    HTMLTimer.innerHTML = "00:00"
+
+    times.appendChild(timerIcon);
+    times.appendChild(HTMLTimer);
+
+    
+    let bestTime = document.createElement('p');
+    let bestTimeIcon = document.createElement('i');
+
+    bestTimeIcon.classList.add('fa-solid', 'fa-trophy');
+
+    if (localStorage.getItem("bestTime") == null) {
+        bestTime.innerHTML = "00:00";
+    } else {
+        bestTime.innerHTML = localStorage.getItem("bestTime");
+    }
+    
+    times.appendChild(bestTimeIcon);
+    times.appendChild(bestTime);
+
+    document.body.appendChild(times);
+
+    startTimer();
+}
+
+
+function setBestTime() {
+    if (localStorage.getItem('bestTime') != null) {
+        let finalTime = fullTime.split(':');
+        let seconds = parseInt(finalTime[1]);
+        let minutes = parseInt(finalTime[0]);
+
+        let bestTime = localStorage.getItem('bestTime').split(':');
+        let bestSeconds = parseInt(bestTime[1]);
+        let bestMinutes = parseInt(bestTime[0]);
+
+        if (minutes < bestMinutes) {
+            localStorage.setItem('bestTime', fullTime);
+        } else if (minutes == bestMinutes) {
+            if (seconds < bestSeconds) {
+                localStorage.setItem('bestTime', fullTime);
+            }
+        }
+    } else {
+        localStorage.setItem('bestTime', fullTime);
     }
 }
